ebee43b79eb5426e283f7a4b1018716174f46064
jverasuazo <jaimeverasuazo@gmail.com>
2024-05-01 11:11:38 -0400

    avance

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const { Pool } = require('pg');
-
+const bcrypt = require('bcrypt');
+  
 app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json()); // Si deseas usar el método POST con cuerpo JSON.
@@ -48,7 +49,32 @@ app.get('/all', (req, res) => {
       res.json(results.rows);
     });
   });
- 
+
+  
+
+  
+  app.post('/login', async (req, res) => {
+    const { user_name, password } = req.body;
+  
+    try {
+      const user = await pool.query("SELECT * FROM usuario WHERE user_name = $1", [user_name]);
+  
+      if (user.rows.length > 0) { // Si el usuario existe
+        const userValid = await pool.query("SELECT * FROM usuario WHERE user_name = $1 and password = $2", [user_name,password]);
+        if (userValid.rows.length > 0) { // Si encontramos el usuario y su clave nos logeamos
+          res.json({ success: true, message: "Login successful" });
+        } else {
+          res.status(401).json({ success: false, message: "Invalid credentials" }); // Usuario existe pero no corresponde la clave
+        }
+      } else {
+        res.status(404).json({ success: false, message: "User not found" }); // Usuario no encontrado
+      }
+    } catch (error) {
+      res.status(500).json({ success: false, message: "Database error" });
+    }
+  });
+  
+
 app.listen(3001, () => {
   console.log('API service is running on port', 3001);
 });
